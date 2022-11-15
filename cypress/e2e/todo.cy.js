6afabf3c1ad493e9fb9590af9299edf7cf1aaf0f
tonyap92 <tonpavlo@mail.ru>
2022-11-15 18:38:52 +0300

    update tests

--- cypress/e2e/todo.cy.js
+++ cypress/e2e/todo.cy.js
@@ -1,10 +1,22 @@
-describe("Test - todo creation - on index page", () => {
+import Dom from "../../src/consts/dom.js";
+import DOM from "../../src/consts/dom.js";
+import * as constants from "constants";
+
+describe("Test - todo creation - on index page ", () => {
   before(() => {
     cy.visit("https://local.dev:8888");
   });
 
+  const checkChildrenExist = () =>
+    cy.get('#listOfTodos > li > input[type="checkbox"]').should("exist").should("have.length", 1);
+
+  const createTodo = (text) => {
+    cy.get("#inpTodoTitle").type(text);
+    cy.get("#btnCreateTodo").click();
+  };
+
   it("enter todo text as number and check disabled button", () => {
-    cy.get("#inpTodoTitle").type(123);
+    cy.get(`#${Dom.INPUT_TODO_TITLE}`).type(123);
     cy.get("#btnCreateTodo").should("be.disabled");
     cy.get("#inpTodoTitle").clear();
   });
@@ -14,8 +26,7 @@ describe("Test - todo creation - on index page", () => {
 
     cy.checkInputExistAndEmpty();
 
-    cy.get("#inpTodoTitle").type(TEST_TODO_TEXT);
-    cy.get("#btnCreateTodo").click();
+    createTodo(TEST_TODO_TEXT);
 
     cy.checkInputExistAndEmpty();
 
@@ -24,11 +35,41 @@ describe("Test - todo creation - on index page", () => {
     todoListChildren.should("exist").should("have.length", 1);
     todoListChildren.first().should("contain.text", TEST_TODO_TEXT);
 
-    const checkChildrenExist = () =>
-      cy.get('#listOfTodos > li > input[type="checkbox"]').should("exist").should("have.length", 1);
-
     checkChildrenExist();
     cy.reload(true);
     checkChildrenExist();
+  });
+
+  it("create todo and validate selection rules", () => {
+    ["Todo 1", "Todo 2"].forEach(createTodo);
+
+    cy.get(`#${DOM.INPUT_TODO_TITLE}`).clear();
+
+    const clickOnListItemAndCheckInputValueFromFunctionCall = (listItemIndex, getCompareValue) =>
+      cy
+        .get(`#${DOM.LIST_OF_TODOS}`)
+        .children()
+        .eq(listItemIndex)
+        .click()
+        .then(($child) => {
+          cy.get(`#${DOM.INPUT_TODO_TITLE}`).should("have.value", getCompareValue($child));
+        });
+
+    const getTextFromTodoItemDomElement = ($child) => $child.text().trim();
+
+    clickOnListItemAndCheckInputValueFromFunctionCall(0, getTextFromTodoItemDomElement)
+      .then(() => {
+        clickOnListItemAndCheckInputValueFromFunctionCall(0, () => "");
+      })
+      .then(() => {
+        clickOnListItemAndCheckInputValueFromFunctionCall(1, getTextFromTodoItemDomElement);
+      });
+
+    // clickOnFirstListItem().then(($child) => {
+    //   cy.get(`#${DOM.INPUT_TODO_TITLE}`).should('have.value', $child.text().trim());
+    //   clickOnFirstListItem().then(($child) => {
+    //     cy.get(`#${DOM.INPUT_TODO_TITLE}`).should('have.value', '');
+    //   });
+    // });
   });
 });
---
+++ src/consts/dom.js
@@ -0,0 +1,7 @@
+class DOM {
+  static INPUT_TODO_TITLE = "inpTodoTitle";
+  static BTN_CREATE_TODO = "btnCreateTodo";
+  static LIST_OF_TODOS = "listOfTodos";
+}
+
+export default DOM;
